b901663bcddbfb04b00cf73b373f049e05e9bf8b
Pedro Kauã <pk.bleite@gmail.com>
2023-07-04 20:23:03 -0300

    Checking winner done.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ export default function Board() {
   const [whatComes, setWhatComes] = useState(true);
   const fillSquare = ( i ) =>  {
 
+    if(square[i] || calculateWinner(square)) {
+      return; //It will check if the values exists, if they don't, it'll cancel this operation.
+    }
+
     const nextSquares = square.slice();
 
     if(nextSquares[i]){
@@ -26,9 +30,42 @@ export default function Board() {
     setSquare(nextSquares);
     setWhatComes(!whatComes)
   }
+  const winner = calculateWinner(square);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (whatComes ? 'X' : 'O');
+    }
+
+  function calculateWinner(squares)  {
+
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for(let i = 0; i < lines.length; i++)  {
+      const [a,b,c] = lines[i] //Will pass the coordinates of each index for a b c. (i.e., 0,1,2 or the first coordinate, will be referred as A,B and C.)
+      
+      if(squares[a] && square[a] === square[b] && square[a] === square[c] ) {
+        return square[a];
+      }
+    }
+    return null;
+  }
 
   return (
     <div className="App">
+      <div className="status">
+        {status}
+      </div>
       <div className="board-row">
           <Square value={square[0]} onSquareClick={() => fillSquare(0)}/>
           <Square value={square[1]} onSquareClick={() => fillSquare(1)}/>
